3b1042c94c754e6deb50b523cd87309b2a5fca18
pallavinarwade <pallavibhutekar1997@gmail.com>
2024-08-06 12:43:07 +0530

    CibilScoreLogic Updated And CibilScoreAlreadyGeneratedExcep Added-Pallavi

---
+++ OE_MicroService/src/main/java/com/oe/customexceptions/CibilScoreAlreadyGeneratedExcep.java
@@ -0,0 +1,9 @@
+package com.oe.customexceptions;
+
+public class CibilScoreAlreadyGeneratedExcep extends RuntimeException{
+
+	public CibilScoreAlreadyGeneratedExcep(String msg) {
+		super(msg);
+	}
+
+}
