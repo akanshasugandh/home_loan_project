28c2c1e932c60bf7ff8248af0c9155123482d0ef
monakhade <ermonakhade@gmail.com>
2024-08-06 15:06:04 +0530

    Logger final All API Done...-BY Mona

--- CRM_MicroService/src/main/java/com/crm/controller/CustomEnqController.java
+++ CRM_MicroService/src/main/java/com/crm/controller/CustomEnqController.java
@@ -1,6 +1,11 @@
 package com.crm.controller;
+import com.crm.CustomeException.AddharCardNumberNotFoundException;
+import com.crm.CustomeException.EmailIdNotFoundException;
+import com.crm.CustomeException.InvalidAgeException;
 import com.crm.CustomeException.LastNameNotFoundExcep;
 import com.crm.CustomeException.NameNotFoundException;
+import com.crm.CustomeException.PancardNotExistException;
+import com.crm.CustomeException.PhonenoNotExistException;
 import com.crm.model.CustomerEnquiry;
 
 import java.sql.Time;
@@ -42,8 +47,6 @@ private static Logger log=LoggerFactory.getLogger(CustomEnqController.class);
 	{
 		ce.setCustomerEnquiryDate(new Date());
 		ce.setCustomerEnquiryTime(new Time(System.currentTimeMillis()));
-		
-
 		servicei.saveEnquiry(ce);
 		EmailDetails ed=new EmailDetails();
 		ed.setToEmail(ce.getEmailId());
@@ -111,32 +114,69 @@ private static Logger log=LoggerFactory.getLogger(CustomEnqController.class);
 	@GetMapping("/getByCustomAge/{age}")
 	public List<CustomerEnquiry> findByAge(@PathVariable int age)
 	{
-		List<CustomerEnquiry>  ce3=servicei.findByAge(age);
+		
 		log.info("info().....get Customer Age()....");
+		log.error("error().....Age Not Found....");
+		try
+		{
+				String s="^(65|[1-9][7-9][0-9]?)$";
+		}
+		catch(InvalidAgeException ag)
+		{
+			log.error(ag.getMessage());
+		}
+		List<CustomerEnquiry>  ce3=servicei.findByAge(age);
 		return ce3;
 	}
 	
 	@GetMapping("/getByEmailId/{emailId}")
 	public CustomerEnquiry getByEmailId(@PathVariable String emailId)
 	{
-		CustomerEnquiry ce=servicei.getByCusEmailId(emailId);
 		log.info("info().....get Customer Email-Id()....");
+		log.error("error().....Customer Email-Id Not Found....");
+		try
+		{
+				String s="^[a-zA-Z0-9. _%+-]+@[a-zA-Z0-9.";
+		}
+		catch(EmailIdNotFoundException ag)
+		{
+			log.error(ag.getMessage());
+		}
+		CustomerEnquiry ce=servicei.getByCusEmailId(emailId);
 		return ce;
 	}
 	
 	@GetMapping("/getCustByContact/{contactNumber}")
 	public CustomerEnquiry getCustByContact(@PathVariable long contactNumber)
 	{
-		CustomerEnquiry ce = servicei.getCustByContact(contactNumber);
 		log.info("info().....get Customer Contact()....");
+		log.error("error().....Customer Contact Not Found....");
+		try
+		{
+				String s="[0-9]+";
+		}
+		catch(PhonenoNotExistException ag)
+		{
+			log.error(ag.getMessage());
+		}
+		CustomerEnquiry ce = servicei.getCustByContact(contactNumber);
 		return ce;
 	}
 	
 	@GetMapping("/getCustByPancard/{pancardNumber}")
 	public CustomerEnquiry getCustByPancard(@PathVariable String pancardNumber)
 	{
-		CustomerEnquiry ce = servicei.getCustByPancard(pancardNumber);
 		log.info("info().....get Customer Pancard()....");
+		log.error("error().....Customer PanCard Not Found....");
+		try
+		{
+				String s="[A-Z]{5}[0-9]{4}[A-Z]{1}";
+		}
+		catch(PancardNotExistException ag)
+		{
+			log.error(ag.getMessage());
+		}
+		CustomerEnquiry ce = servicei.getCustByPancard(pancardNumber);
 		return ce;
 	}
 	@GetMapping("/CustomerEnquiry/{address}")
@@ -165,8 +205,19 @@ private static Logger log=LoggerFactory.getLogger(CustomEnqController.class);
 	@GetMapping("/CustomerEnquiryAadhar/{aadharCardNumber}")
 	public CustomerEnquiry getCustomerEnquiryByAadharCardNumber(@PathVariable ("aadharCardNumber")String aadharCardNumber) 
 	{
-		      CustomerEnquiry c=servicei.getCusEnqByAadharCardNumber(aadharCardNumber);
-		      log.info("info().....get Customer Aadhar()....");
+		log.info("info().....get Customer Aadhar()....");
+		log.error("error().....Customer Aadhar Not Found....");
+		try
+		{
+			String s="^(12|[1-9][7-9][0-9]?)$";
+		}
+		catch(AddharCardNumberNotFoundException ag)
+		{
+			log.error(ag.getMessage());
+		}
+		 
+		CustomerEnquiry c=servicei.getCusEnqByAadharCardNumber(aadharCardNumber);
+		      
 		              return  c;
 	   }
 		
