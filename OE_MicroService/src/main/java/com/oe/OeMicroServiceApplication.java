437b43e232bc028f7065c4b53652fb766b5acf7b
akanshasugandh <akanshasugandh211@gmail.com>
2024-08-09 12:34:23 +0530

    API'S created with names getByCuId, docAccepted, and docRejected in OE

--- OE_MicroService/src/main/java/com/oe/OeMicroServiceApplication.java
+++ OE_MicroService/src/main/java/com/oe/OeMicroServiceApplication.java
@@ -2,6 +2,8 @@ package com.oe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OeMicroServiceApplication {
@@ -9,6 +11,12 @@ public class OeMicroServiceApplication {
 	public static void main(String[] args) {
 		System.out.println("MicroService for OE");
 		SpringApplication.run(OeMicroServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate rsTemplate() {
+		RestTemplate rs=new RestTemplate();
+		return rs;
 	}
 
 }
--- OE_MicroService/src/main/java/com/oe/servicei/CustomerEnquiryServiceI.java
+++ OE_MicroService/src/main/java/com/oe/servicei/CustomerEnquiryServiceI.java
@@ -6,4 +6,8 @@ public interface CustomerEnquiryServiceI {
 
 	public CustomerEnquiry calculateCibilScore(int customerEnquiryId);
 
+	public CustomerEnquiry getByCuId(int customerEnquiryId);
+
+	public void saveEnquiry(CustomerEnquiry ce);
+
 }
